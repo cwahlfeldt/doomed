13f1cf32f3c349c2ee4478928040e5837c37e183
cwahlfeldt <chriswahlfeldt@gmail.com>
2017-12-29 09:58:43 -0600

    date control

--- src/js/cms.js
+++ src/js/cms.js
@@ -1,7 +1,6 @@
 import React from "react";
 import CMS from "netlify-cms";
 import { SketchPicker } from 'react-color';
-
 import PostPreview from "./cms-preview-templates/post";
 
 // Example of creating a custom color widget
@@ -12,6 +11,20 @@ class ColorControl extends React.Component {
         class="color-control"
         style={{ height: "80px" }}
         type="color"
+        value={this.props.value}
+        onInput={(e) => this.props.onChange(e.target.value)}
+      />
+    );
+  }
+}
+
+class DateControl extends React.Component {
+  render() {
+    return (
+      <input
+        class="date-control"
+        style={{ height: "80px" }}
+        type="date"
         value={this.props.value}
         onInput={(e) => this.props.onChange(e.target.value)}
       />
@@ -32,4 +45,5 @@ class SketchControl extends React.Component {
 CMS.registerPreviewStyle("/css/main.css");
 CMS.registerPreviewTemplate("posts", PostPreview);
 CMS.registerWidget("color", ColorControl);
+CMS.registerWidget("date", DateControl);
 CMS.registerWidget("sketch", SketchControl);

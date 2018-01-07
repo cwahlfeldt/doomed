c4cb7ff2cde42b92cc28bf8c60f81cfc658648cb
cwahlfeldt <chriswahlfeldt@gmail.com>
2018-01-07 10:09:26 -0600

    editor edits

--- src/js/cms.js
+++ src/js/cms.js
@@ -89,7 +89,7 @@ CMS.registerEditorComponent({
     }
   ],
   // Pattern to identify a block as being an instance of this component
-  pattern: /^img (\S+)$/,
+  pattern: /^image (\S+)$/,
   // Function to extract data elements from the regexp match
   fromBlock: function(match) {
     return {
@@ -98,13 +98,13 @@ CMS.registerEditorComponent({
   },
   // Function to create a text block from an instance of this component
   toBlock: function(obj) {
-    return 'youtube ' + obj.id;
+    return 'img ' + obj.id;
   },
   // Preview output for this component. Can either be a string or a React component
   // (component gives better render performance)
   toPreview: function(obj) {
     return (
-      `<img src="${obj.id}" alt="${obj.alt}"/>`
+      `<img src="${obj.image}" alt="${obj.alt}"/>`
     );
   }
 });
